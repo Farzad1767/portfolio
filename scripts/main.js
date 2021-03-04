378d3755a767353b6e8f049296596ea26421166d
Farzad1767 <78931322+Farzad1767@users.noreply.github.com>
2021-03-04 15:33:05 -0500

    variety of changes
    made a new and improved button, change color of the caption and experimented on shortcuts

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,28 @@
 let firstname = 'farzad';
 
 console.log(firstname);
+
+function lights(){
+
+    let color = prompt("What color is the light now");
+
+    if (color === "red") {
+        console.log("STOP!");
+    }
+    else if (color === "green") {
+        console.log("GO!");
+    }
+    else if (color === "amber") {
+        console.log("Stop if it is safe to do so");
+    }
+    else {
+        console.log("invalid input");
+    }
+}
+// lights();
+
+function welcome() {
+    let studentname = prompt("what is your name?")
+    let message = "Welcome, " + studentname;
+    document.getElementById('saywelcome').innerHTML = message;
+}
