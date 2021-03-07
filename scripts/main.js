77fc7e68efca2f8de6702fe3bb4492aceec99b15
Farzad1767 <78931322+Farzad1767@users.noreply.github.com>
2021-03-07 18:27:07 -0500

    removed useless images and inserted new images

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
 
+// saying name for console
+
 let firstname = 'farzad';
 
 console.log(firstname);
+
+// lights function
 
 function lights(){
 
@@ -27,3 +31,34 @@ function welcome() {
     let message = "Welcome, " + studentname;
     document.getElementById('saywelcome').innerHTML = message;
 }
+
+// function for buttons
+
+function f1() {
+    alert("Script 1 in progress..");
+}
+function f2() {
+    alert("Script 2 in progress..");
+}
+function f3() {
+    alert("Script 3 in progress..");
+}
+function f4() {
+    alert("Script 4 in progress..");
+}
+function f5() {
+    alert("Script 5 in progress..");
+}
+function f6() {
+    alert("Script 6 in progress..");
+}
+function f7() {
+    alert("Script 7 in progress..");
+}
+function f8() {
+    alert("Script 8 in progress..");
+}
+function f9() {
+    alert("Script 9 in progress..");
+}
+
