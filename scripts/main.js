c9a58a1b3dfed5f3e1bc655367250848a65afdf6
Farzad1767 <78931322+Farzad1767@users.noreply.github.com>
2021-03-19 13:01:08 -0400

    buttons
    changed few buttons to work with a purpose

--- scripts/main.js
+++ scripts/main.js
@@ -34,16 +34,72 @@ function welcome() {
 
 // function for buttons
 
-function f1() {
-    alert("Script 1 in progress..");
+function largerNumber() {
+    let num1 = prompt("Enter the first number");
+    let num2 = prompt("Enter the second number");
+    if (num1 > num2){
+        output = num1;
+    } else if (num2 == num1){
+        output = "The numbers entered are equal";
+    } else if (num2 > num1){
+        output = num2;
+    } else {
+        output = "Invalid input";
+    }
+    document.getElementById('largerNum').innerHTML = output;
 }
-function f2() {
-    alert("Script 2 in progress..");
+function signOfProduct(){
+    let x = parseInt(prompt("Enter the first number"));
+    let y = parseInt(prompt("Enter the second number"));
+    let z = parseInt(prompt("Enter the third number"));
+    
+    if (x>0 && y>0 && z>0){
+        document.getElementById('signOfProduct').innerHTML = "The sign is +";
+    }
+    else if (x==0 && y==0 && z==0){
+        document.getElementById('signOfProduct').innerHTML = "Invalid Input";
+    }
+    else if (x<0 && y<0 && z>0){
+        document.getElementById('signOfProduct').innerHTML = "The sign is +";
+    }
+    else if (x>0 && y<0 && z<0){
+        document.getElementById('signOfProduct').innerHTML = "The sign is +";
+    }
+    else if (x<0 && y>0 && z<0){
+        document.getElementById('signOfProduct').innerHTML = "The sign is +";
+    }
+    else{
+        document.getElementById('signOfProduct').innerHTML = "The sign is -";
+    }
 }
-function f3() {
-    alert("Script 3 in progress..");
+
+function threeNumberSort() {
+    let num1 = parseInt(prompt("Enter the first number"));
+    let num2 = parseInt(prompt("Enter the second number"));
+    let num3 = parseInt(prompt("Enter the third number"));
+    let numsorted = "";
+    if (num1 > num2 && num2 > num3){
+        numsorted = toString(num1) + "," + toString(num2) + "," + toString(num3);
+    }
+    else if (num1 > num3 && num3 > num2){
+        numsorted = num2.toString() + "," + num3.toString() + "," + num1.toString();
+    }
+    else if (num2 > num1 && num1 > num3){
+        numsorted = num3.toString() + "," + num1.toString() + "," + num2.toString();
+    }
+    else if (num2 > num3 && num3 > num1){
+        numsorted = num1.toString() + "," + num3.toString() + "," + num2.toString();
+    }
+    else if (num3 > num2 &&  num2 > num1){
+        numsorted = num1.toString() + "," + num2.toString() + "," + num3.toString();
+    }
+    else if (num3 > num1 && num1 > num2){
+        numsorted = num2.toString() + "," + num1.toString() + "," + num3.toString();
+    }
+    document.getElementById('threeNumberSort').innerHTML = numsorted;
 }
-function f4() {
+
+function forSumThrees() {
     alert("Script 4 in progress..");
 }
 function f5() {
